8472749271663cd27d192f1542c99de7d3e87bc6
user.name like: "Karan <user.email like: "girikaran630@gmail.com>
2026-09-13 00:01:49 +0530

    completed auth logout flow

--- server/src/controller/auth.controller.js
+++ server/src/controller/auth.controller.js
@@ -49,7 +49,6 @@ export const login = async (req, res) => {
   try {
     const { email, password } = req.body;
 
-    
     if (!email || !password) {
       return res.status(400).json({
         success: false,
@@ -57,7 +56,6 @@ export const login = async (req, res) => {
       });
     }
 
-    
     const user = await User.findOne({ email });
 
     if (!user) {
@@ -67,11 +65,7 @@ export const login = async (req, res) => {
       });
     }
 
-    
-    const isMatch = await bcrypt.compare(
-      password,
-      user.password
-    );
+    const isMatch = await bcrypt.compare(password, user.password);
 
     if (!isMatch) {
       return res.status(400).json({
@@ -80,16 +74,11 @@ export const login = async (req, res) => {
       });
     }
 
-    
-    const { accessToken, refreshToken } = generateTokens(
-      user._id
-    );
+    const { accessToken, refreshToken } = generateTokens(user._id);
 
-    
     user.refreshToken = refreshToken;
     await user.save();
 
-    
     res.cookie("accessToken", accessToken, {
       httpOnly: true,
       secure: false,
@@ -104,7 +93,6 @@ export const login = async (req, res) => {
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
-    
     return res.status(200).json({
       success: true,
       message: "Login successful",
@@ -124,6 +112,34 @@ export const login = async (req, res) => {
     return res.status(500).json({
       success: false,
       message: "Internal Server Error",
+    });
+  }
+};
+
+export const logout = async (req, res) => {
+  try {
+    const refreshToken = req.cookies.refreshToken;
+
+    if (refreshToken) {
+      const user = await User.findOne({ refreshToken });
+
+      if (user) {
+        user.refreshToken = "";
+        await user.save();
+      }
+    }
+
+    res.clearCookie("accessToken");
+    res.clearCookie("refreshToken");
+
+    return res.status(200).json({
+      success: true,
+      message: "Logout successful",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: true,
+      message: "internal server error",
     });
   }
 };
