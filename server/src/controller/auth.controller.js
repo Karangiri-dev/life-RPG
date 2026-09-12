344a92f135f0920ea4820a2047a24e38aff6453d
user.name like: "Karan <user.email like: "girikaran630@gmail.com>
2026-09-13 01:03:07 +0530

    completed authentication system

--- server/src/controller/auth.controller.js
+++ server/src/controller/auth.controller.js
@@ -1,6 +1,8 @@
 import User from "../models/auth.model.js";
 import bcrypt from "bcryptjs";
 import { generateTokens } from "../utils/generate.js";
+import config from "../config/config.js";
+import jwt from "jsonwebtoken";
 
 export const register = async (req, res) => {
   try {
@@ -140,6 +142,88 @@ export const logout = async (req, res) => {
     return res.status(500).json({
       success: true,
       message: "internal server error",
+    });
+  }
+};
+
+export const refreshAccessToken = async (req, res) => {
+  try {
+    const refreshToken = req.cookies.refreshToken;
+
+    if (!refreshToken) {
+      return res.status(401).json({
+        success: false,
+        message: "refresh token not found",
+      });
+    }
+
+    const user = await User.findOne({
+      refreshToken,
+    });
+
+    if (!user) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid refresh token",
+      });
+    }
+
+    const decoded = jwt.verify(refreshToken, config.REFRESH_TOKEN_SECRET);
+
+    const accessToken = jwt.sign(
+      {
+        id: decoded.id,
+      },
+      config.ACCESS_TOKEN_SECRET,
+      {
+        expiresIn: "15m",
+      },
+    );
+
+    res.cookie("accessToken", accessToken, {
+      httpOnly: true,
+      secure: false,
+      sameSite: "strict",
+      maxAge: 15 * 60 * 1000,
+    });
+
+    return res.status(200).json({
+      success: true,
+      message: "New access token generated",
+    });
+  } catch (error) {
+    console.log(error);
+
+    return res.status(500).json({
+      success: false,
+      message: "internal server error",
+    });
+  }
+};
+
+export const getCurrentUser = async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select(
+      "-password -refreshToken",
+    );
+
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "user not found",
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      user,
+    });
+  } catch (error) {
+    console.log(error);
+
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
     });
   }
 };
--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -1,5 +1,12 @@
 import { Router } from "express";
-import { login, logout, register } from "../controller/auth.controller.js";
+import {
+    getCurrentUser,
+  login,
+  logout,
+  refreshAccessToken,
+  register,
+} from "../controller/auth.controller.js";
+import authMiddleware from "../middleware/auth.middleware.js";
 
 const router = Router();
 
@@ -8,5 +15,9 @@ router.post("/register", register);
 router.post("/login", login);
 
 router.post("/logout", logout);
+
+router.post("/refresh-token", refreshAccessToken);
+
+router.get("/me", authMiddleware, getCurrentUser);
 
 export default router;
